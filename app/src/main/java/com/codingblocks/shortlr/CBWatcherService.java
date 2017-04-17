f3478f20c452d1eae0a29c19b3d63b69989557c2
Piyush Ajmani <piyushajmani5@gmail.com>
2017-04-17 15:51:32 +0530

    Added a window when clip is url
    Signed-off-by: Piyush Ajmani <piyushajmani5@gmail.com>

--- app/src/main/java/com/codingblocks/shortlr/CBWatcherService.java
+++ app/src/main/java/com/codingblocks/shortlr/CBWatcherService.java
@@ -4,21 +4,31 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Calendar;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import android.app.Service;
 import android.content.ClipData;
 import android.content.ClipDescription;
 import android.content.ClipboardManager;
 import android.content.ClipboardManager.OnPrimaryClipChangedListener;
+import android.content.Context;
 import android.content.Intent;
 import android.os.IBinder;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.Toast;
 
 
 public class CBWatcherService extends Service {
     private final String TAG = "MyWatcherService";
+    public static final String URL_REGEX = "^((https?|ftp)://|(www|ftp)\\.)?[a-z0-9-]+(\\.[a-z0-9-]+)+([/?].*)?$";
     private OnPrimaryClipChangedListener listener = new OnPrimaryClipChangedListener() {
         public void onPrimaryClipChanged() {
             performClipboardCheck();
@@ -45,10 +55,52 @@ public class CBWatcherService extends Service {
         ClipboardManager cb = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
         if (cb.hasPrimaryClip()) {
             ClipData cd = cb.getPrimaryClip();
-            Log.d(TAG, "performClipboardCheck: ");
+
             if (cd.getDescription().hasMimeType(ClipDescription.MIMETYPE_TEXT_PLAIN)) {
-                Toast.makeText(this, cd.getItemAt(0).getText().toString(), Toast.LENGTH_SHORT).show();
+                String clipboardText = cd.getItemAt(0).getText().toString();
+                Pattern p = Pattern.compile(URL_REGEX);
+                Matcher m = p.matcher(clipboardText);
+                if (m.find()) {
+                    showView();
+
+                } else {
+
+                }
+
             }
         }
     }
+
+    public void showView() {
+        final WindowManager manager = (WindowManager) getApplicationContext().getSystemService(Context.WINDOW_SERVICE);
+        WindowManager.LayoutParams layoutParams = new WindowManager.LayoutParams();
+        layoutParams.gravity = Gravity.CENTER;
+        layoutParams.type = WindowManager.LayoutParams.TYPE_SYSTEM_ALERT;
+        layoutParams.width = WindowManager.LayoutParams.WRAP_CONTENT;
+        layoutParams.height = WindowManager.LayoutParams.WRAP_CONTENT;
+        layoutParams.alpha = 1.0f;
+        layoutParams.packageName = getPackageName();
+        layoutParams.buttonBrightness = 1f;
+        layoutParams.windowAnimations = android.R.style.Animation_Dialog;
+
+        final View view = View.inflate(getApplicationContext(), R.layout.test_layout, null);
+        Button yesButton = (Button) view.findViewById(R.id.yesButton);
+        Button noButton = (Button) view.findViewById(R.id.noButton);
+        yesButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                manager.removeView(view);
+            }
+        });
+        noButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                manager.removeView(view);
+            }
+        });
+        manager.addView(view, layoutParams);
+    }
 }
+
